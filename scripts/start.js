9dfcedc4c8ad26dabaa30869523707314cdcf4ad
pigwin <peder.vatn.austad@gmail.com>
2026-03-11 08:53:09 +0000

    yez - looking slightly betta yez?

--- scripts/start.js
+++ scripts/start.js
@@ -10,7 +10,7 @@ dataService.getCategories()
         console.log('loaded categories in ' + loadTime + ' ms');
         if (data.length > 0) {
             //makes the top of the website
-            var Temp = '<h1 class="title">4pba ultimate quiz platform</h1><p style="text-align: center;">Håper du er fornøyd nå Viliam</p><div class="top1">';
+            var Temp = '<h1 class="title">4pba ultimate quiz platform</h1><p class="subtitle">Håper du er fornøyd nå Viliam</p><div class="top1">';
             //repeats this until all of the data is in boxes
             data.forEach((itemData) => {
                 Temp += '<div class="box">';
